c3df17c7df73ccb07582f7f9d8c8578da25e0a91
Simon Mutch <smutch@unimelb.edu.au>
2021-11-08 20:29:33 +1100

    4

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
 use palette::Srgb;
 use indicatif::ProgressBar;
+use nalgebra_glm::{Vec3, vec3};
+
+fn ray_color(r: Vec3) -> Srgb {
+    let direction = r.normalize();
+    let t = 0.5 * (direction.y + 1.0);
+    let components = (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    Srgb::new(components[0], components[1], components[2])
+}
+
+struct Camera {
+    origin: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+    lower_left_corner: Vec3
+}
+
+impl Camera {
+    fn new(aspect: f32) -> Self {
+        let viewport_height = 2.0;
+        let viewport_width = aspect * viewport_height;
+        let focal_length = 1.0;
+        let origin = vec3(0.0, 0.0, 0.0);
+        let horizontal = vec3(viewport_width, 0.0, 0.0);
+        let vertical = vec3(0.0, viewport_height, 0.0);
+        let lower_left_corner = origin - horizontal * 0.5 - vertical * 0.5 - vec3(0.0, 0.0, focal_length);
+        Self { origin, horizontal, vertical, lower_left_corner }
+    }
+}
 
 fn main() -> Result<(), image::ImageError> {
-    const WIDTH: u32 = 256;
-    const HEIGHT: u32 = 256;
+    const ASPECT: f32 = 16.0 / 9.0;
+    const WIDTH: u32 = 400;
+    const HEIGHT: u32 = (WIDTH as f32 / ASPECT) as u32;
 
+    let camera = Camera::new(ASPECT);
     let mut framebuffer = image::RgbImage::new(WIDTH, HEIGHT);
 
     let pbar = ProgressBar::new(HEIGHT as u64);
     for jj in 0..HEIGHT {
         for ii in 0..WIDTH {
-            let color = Srgb::new(ii as f32 / (WIDTH - 1) as f32, jj as f32 / (HEIGHT -1) as f32, 0.25).into_format().into();
+            let u = ii as f32 / (WIDTH - 1) as f32;
+            let v = jj as f32 / (HEIGHT -1) as f32;
+            let ray = camera.lower_left_corner + u*camera.horizontal + v*camera.vertical - camera.origin;
+            let color = ray_color(ray).into_format().into();
             framebuffer.put_pixel(ii, HEIGHT-jj-1, image::Rgb(color));
         } 
         pbar.inc(1);
