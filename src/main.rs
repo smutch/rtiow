eb4a686c76edaa4ce5cdbe3f34b9fab2ef920148
Simon Mutch <smutch@unimelb.edu.au>
2021-11-08 19:53:20 +1100

    2.3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use palette::Srgb;
+use indicatif::ProgressBar;
 
 fn main() -> Result<(), image::ImageError> {
     const WIDTH: u32 = 256;
@@ -6,12 +7,15 @@ fn main() -> Result<(), image::ImageError> {
 
     let mut framebuffer = image::RgbImage::new(WIDTH, HEIGHT);
 
+    let pbar = ProgressBar::new(HEIGHT as u64);
     for jj in 0..HEIGHT {
         for ii in 0..WIDTH {
             let color = Srgb::new(ii as f32 / (WIDTH - 1) as f32, jj as f32 / (HEIGHT -1) as f32, 0.25).into_format().into();
             framebuffer.put_pixel(ii, HEIGHT-jj-1, image::Rgb(color));
         } 
+        pbar.inc(1);
     }
+    pbar.finish_with_message("render complete");
 
     framebuffer.save("./image.png")?;
 
