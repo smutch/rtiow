568c71dfa0e8e59cb7770b24f3cf9f868b3e7e71
Simon Mutch <smutch@unimelb.edu.au>
2021-11-09 16:29:14 +1100

    6.1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use palette::Srgb;
 use indicatif::ProgressBar;
-use nalgebra_glm::{Vec3, vec3};
+use nalgebra_glm::{vec3, Vec3};
+use palette::Srgb;
 
 fn ray_color(r: &Vec3, origin: &Vec3) -> Srgb {
-    if hit_sphere(&vec3(0.0, 0.0, -1.0), 0.5, r, origin) {
-        return Srgb::new(1.0, 0.0, 0.0);
-    }
-    let direction = r.normalize();
-    let t = 0.5 * (direction.y + 1.0);
-    let components = (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    let components = match hit_sphere(&vec3(0.0, 0.0, -1.0), 0.5, r, origin) {
+        None => {
+            let direction = r.normalize();
+            let t = 0.5 * (direction.y + 1.0);
+            (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0)
+        }
+        Some(t) => {
+            let n = ((origin + r * t) - vec3(0.0, 0.0, -1.0)).normalize();
+            0.5 * vec3(n.x + 1.0, n.y + 1.0, n.z + 1.0)
+        }
+    };
     Srgb::new(components[0], components[1], components[2])
 }
 
@@ -16,7 +21,7 @@ struct Camera {
     origin: Vec3,
     horizontal: Vec3,
     vertical: Vec3,
-    lower_left_corner: Vec3
+    lower_left_corner: Vec3,
 }
 
 impl Camera {
@@ -27,18 +32,29 @@ impl Camera {
         let origin = vec3(0.0, 0.0, 0.0);
         let horizontal = vec3(viewport_width, 0.0, 0.0);
         let vertical = vec3(0.0, viewport_height, 0.0);
-        let lower_left_corner = origin - horizontal * 0.5 - vertical * 0.5 - vec3(0.0, 0.0, focal_length);
-        Self { origin, horizontal, vertical, lower_left_corner }
+        let lower_left_corner =
+            origin - horizontal * 0.5 - vertical * 0.5 - vec3(0.0, 0.0, focal_length);
+        Self {
+            origin,
+            horizontal,
+            vertical,
+            lower_left_corner,
+        }
     }
 }
 
-fn hit_sphere(centre: &Vec3, radius: f32, ray: &Vec3, origin: &Vec3) -> bool {
+fn hit_sphere(centre: &Vec3, radius: f32, ray: &Vec3, origin: &Vec3) -> Option<f32> {
     let oc = origin - centre;
     let a = ray.dot(ray);
     let b = 2.0 * oc.dot(ray);
     let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b*b - 4.0*a*c;
-    discriminant > 0.0
+    let discriminant = b * b - 4.0 * a * c;
+
+    if discriminant < 0.0 {
+        None
+    } else {
+        Some((-b - discriminant.sqrt()) / (2.0 * a))
+    }
 }
 
 fn main() -> Result<(), image::ImageError> {
@@ -53,11 +69,12 @@ fn main() -> Result<(), image::ImageError> {
     for jj in 0..HEIGHT {
         for ii in 0..WIDTH {
             let u = ii as f32 / (WIDTH - 1) as f32;
-            let v = jj as f32 / (HEIGHT -1) as f32;
-            let ray = camera.lower_left_corner + u*camera.horizontal + v*camera.vertical - camera.origin;
+            let v = jj as f32 / (HEIGHT - 1) as f32;
+            let ray = camera.lower_left_corner + u * camera.horizontal + v * camera.vertical
+                - camera.origin;
             let color = ray_color(&ray, &camera.origin).into_format().into();
-            framebuffer.put_pixel(ii, HEIGHT-jj-1, image::Rgb(color));
-        } 
+            framebuffer.put_pixel(ii, HEIGHT - jj - 1, image::Rgb(color));
+        }
         pbar.inc(1);
     }
     pbar.finish_with_message("render complete");
@@ -65,5 +82,4 @@ fn main() -> Result<(), image::ImageError> {
     framebuffer.save("./image.png")?;
 
     Ok(())
-
 }
