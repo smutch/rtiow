57d66468a02092349e184ba7dba72dde9076da6a
Simon Mutch <smutch@unimelb.edu.au>
2021-11-09 15:30:34 +1100

    5.2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use palette::Srgb;
 use indicatif::ProgressBar;
 use nalgebra_glm::{Vec3, vec3};
 
-fn ray_color(r: Vec3) -> Srgb {
+fn ray_color(r: &Vec3, origin: &Vec3) -> Srgb {
+    if hit_sphere(&vec3(0.0, 0.0, -1.0), 0.5, r, origin) {
+        return Srgb::new(1.0, 0.0, 0.0);
+    }
     let direction = r.normalize();
     let t = 0.5 * (direction.y + 1.0);
     let components = (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
@@ -29,6 +32,15 @@ impl Camera {
     }
 }
 
+fn hit_sphere(centre: &Vec3, radius: f32, ray: &Vec3, origin: &Vec3) -> bool {
+    let oc = origin - centre;
+    let a = ray.dot(ray);
+    let b = 2.0 * oc.dot(ray);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant > 0.0
+}
+
 fn main() -> Result<(), image::ImageError> {
     const ASPECT: f32 = 16.0 / 9.0;
     const WIDTH: u32 = 400;
@@ -43,7 +55,7 @@ fn main() -> Result<(), image::ImageError> {
             let u = ii as f32 / (WIDTH - 1) as f32;
             let v = jj as f32 / (HEIGHT -1) as f32;
             let ray = camera.lower_left_corner + u*camera.horizontal + v*camera.vertical - camera.origin;
-            let color = ray_color(ray).into_format().into();
+            let color = ray_color(&ray, &camera.origin).into_format().into();
             framebuffer.put_pixel(ii, HEIGHT-jj-1, image::Rgb(color));
         } 
         pbar.inc(1);
